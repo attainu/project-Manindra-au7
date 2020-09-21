dd42e6fdbad8bff8ef702ad0377528ae6fa90dd8
Manindra-au7 <manindra8@gmail.com>
2020-09-21 15:38:35 +0530

    added database and connected to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 require("dotenv").config()
 const express =  require("express")
+const mongoose = require('mongoose')
+
+
 const app = express()
 
+//database connection
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+})
+.then(()=>console.log("mongodb connected"))
+.catch(err=>console.log("error connecting TO mONGO DB"))
 
+//routes
 app.get("/",(req,res) => {
     res.send("This is the home page")
 })
