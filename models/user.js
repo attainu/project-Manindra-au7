719d2d961f29768efb621fb1a8ddcb46e12836c8
Manindra-au7 <manindra8@gmail.com>
2020-09-24 02:17:56 +0530

    update usermodel

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 const uuidv1 = require('uuid/v1')
 
 const userSchema = new mongoose.Schema({
@@ -10,18 +10,18 @@ const userSchema = new mongoose.Schema({
         maxlength:32
     },
     email: {
-        type:String,
-        trim:true,
-        required:true,
-        maxlength:32
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
     }, 
     hashed_password: {
         type: String,
-        required:true
+        required: true
     },
     about: {
         type: String,
-        trim:true
+        trim: true
     },
     salt: String,
     role: {
@@ -33,5 +33,34 @@ const userSchema = new mongoose.Schema({
         default: []
     }
 },{
-   timestamps:true 
+   timestamps: true 
 })
+
+
+userSchema.virtual('password')
+.set(function(password){
+    this.password = password
+    this.salt=uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(){
+    return this._password
+})
+
+userSchema.methods = {
+    encryptPassword : function (password) {
+        if (!password) return ' ';
+        try {
+            return crypto.createHmac('sha1' , this.salt)
+                .update(password)
+                .digest('hex')
+
+        }
+        catch (err) {
+            return "";
+        }
+    }
+}
+
+
+module.exports = mongoose.model("User",userSchema)
