1cef5570c2ba9e186902e673d7ec594efe7159c5
Manindra-au7 <manindra8@gmail.com>
2020-10-02 14:41:56 +0530

    add .env file and removed some warnings in console

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const products = require("./data/products");
 
@@ -16,4 +17,5 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server is running at 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is running at ${PORT}`));
