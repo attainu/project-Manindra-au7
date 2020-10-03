3cdf89590262add0c5b60f2b96a500032546258a
Manindra-au7 <manindra8@gmail.com>
2020-10-03 17:21:36 +0530

    registrating the user and hash password and save

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-// @desc    Auth user & get token  POST /api/users/login Public
+//Auth user & get tokenPOST /api/users/login Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -22,7 +22,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-//   Get user profile GET /api/users/profile Private
+// Register a new user  POST /api/users Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc    Get user profile GET /api/users/profile Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -39,4 +70,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
