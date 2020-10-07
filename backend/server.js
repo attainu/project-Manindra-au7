5390af02e0f07cee7f6f2901a36c3df5f412fec5
Manindra-au7 <manindra8@gmail.com>
2020-10-07 17:49:50 +0530

    admin

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from 'morgan'
 import path from "path";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -14,6 +15,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json());
 
